5a665171df359c09bf5cec89aa23e8fbd65b76cf
Drakoxw <dragon12xw@gmail.com>
2024-05-18 02:41:44 -0500

    feat: recover ok

--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"database/sql"
 	"log"
+
 	"time"
 
 	_ "github.com/go-sql-driver/mysql"
@@ -14,11 +15,12 @@ import (
 	"github.com/Drakoxw/go-cities-service/internal/cities/utils"
 	"github.com/Drakoxw/go-cities-service/internal/database"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
-	// dsn := "root:@tcp(localhost:3307)/drakodb" // conexion local
-	dsn := "root:@tcp(mysql:3306)/drakodb"
+	dsn := "root:@tcp(localhost:3307)/drakodb" // conexion local
+	// dsn := "root:@tcp(mysql:3306)/drakodb"
 	db, err := sql.Open("mysql", dsn)
 	if err != nil {
 		log.Fatal(err)
@@ -29,28 +31,26 @@ func main() {
 	database.LoadData(db)
 
 	e := echo.New()
+	e.Use(middleware.Recover())
 
 	cityRepo := mysql.NewMySQLCityRepository(db)
 	cityUC := usecase.NewCityUseCase(cityRepo)
 	handlers.NewCityHandler(e, cityUC)
 
-	port := utils.GetPort()
-	go func() {
-		log.Fatal(e.Start(port))
-	}()
-
-	cron := time.NewTicker(12 * time.Hour)
+	cron := time.NewTicker(30 * time.Second)
 	go func() {
 		for {
 			select {
 			case <-cron.C:
 				err = crones.UpdateCities(cityUC)
 				if err != nil {
-					log.Println("❌❌" + err.Error() + "❌❌")
+					// log.Println("❌❌" + err.Error() + "❌❌")
 				}
+				log.Println("Datos actualizados")
 			}
 		}
 	}()
 
-	select {}
+	port := utils.GetPort()
+	log.Fatal(e.Start(port))
 }
--- go.mod
+++ go.mod
@@ -8,6 +8,11 @@ require (
 )
 
 require (
+	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
+	golang.org/x/time v0.5.0 // indirect
+)
+
+require (
 	filippo.io/edwards25519 v1.1.0 // indirect
 	github.com/labstack/gommon v0.4.2 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
