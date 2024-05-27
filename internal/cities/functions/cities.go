fbcafbf0ad81a7dd585519bc53d443e852a6fd3e
Drakoxw <dragon12xw@gmail.com>
2024-05-26 20:24:21 -0500

    feat: se corrigen warning capitalize de los errores

--- internal/cities/functions/cities.go
+++ internal/cities/functions/cities.go
@@ -17,54 +17,54 @@ func GetCitiesFromFile() ([]models.City, error) {
 	// Obtener los datos desde la URL
 	resp, err := http.Get(url)
 	if err != nil {
-		return nil, errors.New("No se pudieron obtener los datos desde la URL")
+		return nil, errors.New("no se pudieron obtener los datos desde la URL")
 	}
 	defer resp.Body.Close()
 
 	if resp.StatusCode != http.StatusOK {
-		return nil, errors.New("Respuesta no exitosa desde la URL")
+		return nil, errors.New("respuesta no exitosa desde la URL")
 	}
 
 	var cities []models.City
 	err = json.NewDecoder(resp.Body).Decode(&cities)
 	if err != nil {
-		return nil, errors.New("No se pudieron decodificar los datos")
+		return nil, errors.New("no se pudieron decodificar los datos")
 	}
 
 	// Escribir nuevos datos en el archivo JSON
 	bytes, err := json.MarshalIndent(cities, "", "  ")
 	if err != nil {
-		return nil, errors.New("No se pudieron codificar los datos")
+		return nil, errors.New("no se pudieron codificar los datos")
 	}
 
 	// Se reescribe el archivo tambn por si las dudas
 	err = os.WriteFile("data/cities.json", bytes, 0644)
 	if err != nil {
-		return nil, errors.New("No se pudieron guardar los datos")
+		return nil, errors.New("no se pudieron guardar los datos")
 	}
 
 	return cities, nil
 
 }
 
-func LoadCitiesStart(ruta string) ([]models.City, error) {
-	var ciudades []models.City
+func LoadCitiesStart(pathFile string) ([]models.City, error) {
+	var cities []models.City
 
-	archivo, err := os.Open(ruta)
+	file, err := os.Open(pathFile)
 	if err != nil {
 		return nil, err
 	}
-	defer archivo.Close()
+	defer file.Close()
 
-	bytes, err := io.ReadAll(archivo)
-	if err != nil {
-		return nil, err
-	}
-
-	err = json.Unmarshal(bytes, &ciudades)
+	bytes, err := io.ReadAll(file)
 	if err != nil {
 		return nil, err
 	}
 
-	return ciudades, nil
+	err = json.Unmarshal(bytes, &cities)
+	if err != nil {
+		return nil, err
+	}
+
+	return cities, nil
 }
